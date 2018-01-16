224b751e04fd2e3d4c6ffeff53fd30ed22c32927
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2018-01-15 21:03:59 -0500

    functionality of challenge 1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,19 +2,21 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "",
   database: "bamazon_db"
 });
 
+var itemArr = [];
+// to store the items in the database
+var productArr = [];
+
+var newQuantity;
+
+var id;
 
 connection.connect(function(err) {
   if (err) throw err;
@@ -29,11 +31,89 @@ function readProducts() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
 
+    //clear out the itemArr
+    itemArr = [];
+    productArr = res;
 
     for (var i = 0; i < res.length; i++) {
       console.log(res[i].item_id + " " + res[i].product_name)
+    	itemArr.push(res[i].item_id + " " + res[i].product_name)
     }
-    console.log(res);
+
+    // console.log(res);
+    // console.log("itemArr " + itemArr)
+    promptUser();
   });
  }
+
+ function promptUser(){
+
+	inquirer.prompt([
+		{
+			type: "list",
+      message: "Which item would you like to buy?",
+      name: "item",
+      choices: itemArr
+		},
+		{
+			type: "input",
+      message: "How many units would you like to buy?",
+      name: "amount"
+		}
+	]).then(ans => {
+		
+
+		id = ans.item.substr(0, ans.item.indexOf(' '));
+
+		//the index will be one less than the id
+		
+		//if the store has enough 
+		if (productArr[id-1].stock_quantity >= ans.amount){
+			//calculate what the new quantity will be
+			newQuantity = productArr[id-1].stock_quantity - ans.amount;
+			//update sql database
+			updateProduct();
+				
+			//show the total cost
+			var price = productArr[id-1].price;
+			var totalPrice = price * ans.amount;
+
+	console.log("total cost: $" + totalPrice)
+		}
+			
+		else{
+			console.log("Insufficient quantity! Your order cannot be filled")
+			//insufficient quantity - do nothing
+		}
+
+		console.log(price)
+
+		console.log(id +"id " + id)
+		console.log(id == 1);
+		console.log(id ===1)
+
+		});
+ }
+
+ function updateProduct() {
+  var query = connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        stock_quantity: newQuantity
+      },
+      {
+        item_id: id
+      }
+    ],
+    function(err, res) {
+      console.log(res.affectedRows + " products updated!\n");
+      
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
 
