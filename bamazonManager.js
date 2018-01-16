cbf4e14574da908755197c692989a8885730149a
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2018-01-16 15:50:31 -0500

    added package to log the info in a table

--- bamazonManager.js
+++ bamazonManager.js
@@ -2,6 +2,9 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+var Table = require('cli-table');
+
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -23,12 +26,12 @@ connection.connect(function(err) {
 
 function promptManager(){
 
-inquirer.prompt([
+	inquirer.prompt([
 		{
 			type: "list",
-      message: "What would you like to do",
+      message: "\nWhat would you like to do?",
       name: "menu",
-      choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+      choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
 		}
 	]).then(ans => {
 		
@@ -49,10 +52,14 @@ inquirer.prompt([
 	  	case "Add New Product":
 	  	createProduct();
 	  	break;
+
+	  	case "Exit":
+	  	connection.end();
+	  	break;
 	  }
 
 		});
- }
+ 	}
 
 function readProducts(){
 	console.log("here")
@@ -78,17 +85,25 @@ function readProducts(){
 
 function viewProducts(){
 	var query = connection.query("SELECT * FROM products", function(err, res) {
-    	if (err) throw err;
+    if (err) throw err;
 
 
-    	console.log("Items for Sale:")
-    	console.log("ID  ||  Product    ||    Department  ||   Price   ||   Quantity   ||")
-    	for (var i = 0; i < res.length; i++) {
-    		console.log(res[i].item_id + " || " + res[i].product_name + 
-    			" || " + res[i].department_name + " || " + res[i].price + " || " + res[i].stock_quantity)
-    	}
-    }
-  );
+    console.log("Items for Sale:")
+    
+    // instantiate 
+		var table = new Table({
+    head: ['ID', 'Product', 'Department', 'Price', 'Quantity']
+  	, colWidths: [10, 22, 21, 10, 10]
+		});
+ 
+		for (var i = 0; i < res.length; i++) {
+ 			table.push(
+    	[res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+			);
+ 		} 
+ 		console.log(table.toString());
+ 		promptManager();
+  });
 }
 
 function lowInventory(){
@@ -98,9 +113,21 @@ function lowInventory(){
     function(err, res) {
       // console.log(res.affectedRows + " products updated!\n");
       // Call deleteProduct AFTER the UPDATE completes
-      for (var i = 0; i < res.length; i++) {
-      	console.log(res[i].product_name + " || " + res[i].stock_quantity)
-      }
+      // instantiate 
+		var table = new Table({
+    head: ['Product', 'Quantity']
+  	, colWidths: [22,10]
+		});
+ 
+		for (var i = 0; i < res.length; i++) {
+ 			table.push(
+    	[res[i].product_name, res[i].stock_quantity]
+			);
+ 		} 
+ 		console.log(table.toString());
+
+      
+      promptManager();
       
     }
   );
@@ -159,6 +186,7 @@ function updateInventory(){
     ],
     function(err, res) {
       console.log(res.affectedRows + " products updated!\n");
+      promptManager();
     }
   );
 
@@ -206,11 +234,15 @@ function createProduct() {
     function(err, res) {
       console.log(res.affectedRows + " product inserted!\n");
       // Call updateProduct AFTER the INSERT completes
+
+      // logs the actual query being run
+  console.log(query.sql);
+
+  promptManager();
     }
   );
 
-  // logs the actual query being run
-  console.log(query.sql);
+  
 	});
  
 }
