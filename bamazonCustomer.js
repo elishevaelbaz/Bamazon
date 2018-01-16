8d43d928537956f87fcdcf3f048f014e82cb2abb
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2018-01-16 18:24:28 -0500

    editing indentation and comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,13 +10,8 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-// var itemArr = [];
 // to store the items in the database
 var productArr = [];
-
-var newQuantity;
-
-var id;
 
 connection.connect(function(err) {
   if (err) throw err;
@@ -24,27 +19,19 @@ connection.connect(function(err) {
   // promptUser();
 });
 
-
 function readProducts() {
   console.log("Selecting all products...\n");
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
 
-    //clear out the itemArr
-    // itemArr = [];
     productArr = res;
 
-    console.log("item_id   product_name              price")
-    console.log("-----------------------------------------")
+    console.log(" item_id    product_name             price")
+    console.log(" -----------------------------------------")
     for (var i = 0; i < res.length; i++) {
-    	// var numSpaces = 5 - (i+1) / 10;
-    	// console.log(numSpaces)
-      console.log("  " + res[i].item_id + " ".repeat(9 - res[i].item_id.toString().length) + res[i].product_name + " ".repeat(25-res[i].product_name.length) +  res[i].price)
-    	// itemArr.push(res[i].item_id + " " + res[i].product_name)
+      console.log("   " + res[i].item_id + " ".repeat(9 - res[i].item_id.toString().length) + res[i].product_name + " ".repeat(25-res[i].product_name.length) +  res[i].price)
     }
 
-    // console.log(res);
-    // console.log("itemArr " + itemArr)
     promptUser();
   });
  }
@@ -54,19 +41,18 @@ function readProducts() {
 	inquirer.prompt([
 		{
 			type: "input",
-      message: "\nPlease enter the id of the item you would like to buy.",
+      message: "\n Please enter the id of the item you would like to buy.",
       name: "item",
-      // choices: itemArr,
 
       validate: function (input) {
 
    		// if input is not a number
    		if (isNaN(input)) {
-   			return "Please enter a valid number";
+   			return " Please enter a valid number";
    		}
       // if input is a number but not from 1-10
       else if (parseInt(input) < 1 || parseInt(input) > productArr.length){
-      	return "Please enter a number between 1 and " + productArr.length
+      	return " Please enter a number between 1 and " + productArr.length
       }
       // otherwise
       return true;
@@ -81,7 +67,7 @@ function readProducts() {
 
    		// if input is not a number
    		if (isNaN(input)) {
-   			return "Please enter a valid number";
+   			return " Please enter a valid number";
    		}
       // otherwise
       return true;
@@ -90,63 +76,57 @@ function readProducts() {
 	]).then(ans => {
 		
 
-		// id = ans.item.substr(0, ans.item.indexOf(' '));
-		id = ans.item;
+		var id = ans.item;
+		var amt = ans.amount
 
-		//the index will be one less than the id
+		//the index in productArr will be one less than the id
 		
 		//if the store has enough 
 		if (productArr[id-1].stock_quantity >= ans.amount){
 			//calculate what the new quantity will be
-			newQuantity = productArr[id-1].stock_quantity - ans.amount;
+			var newQuantity = productArr[id-1].stock_quantity - ans.amount;
 			
-				
-			//show the total cost
-			var price = productArr[id-1].price;
-			var totalPrice = price * ans.amount;
-
-	console.log("total cost: $" + totalPrice.toFixed(2))
-
-	//update sql database
-			updateProduct();
-
+			//update sql database and show the cost
+			updateProduct(newQuantity, id, amt);
 		}
 			
 		else{
-			console.log("Insufficient quantity! Your order cannot be filled.")
+			console.log(" Insufficient quantity! Your order cannot be filled.")
 			exitOrCont();
 		}
 
 	});
  }
 
- function updateProduct() {
+function updateProduct(q, i, a) {
   var query = connection.query(
     "UPDATE products SET ? WHERE ?",
     [
       {
-        stock_quantity: newQuantity
+        stock_quantity: q
       },
       {
-        item_id: id
+        item_id: i
       }
     ],
     function(err, res) {
-      console.log(res.affectedRows + " products updated!\n");
-      exitOrCont();
-    }
-  );
+      console.log(" Purchase Successful!");
 
-  // logs the actual query being run
-  console.log(query.sql);
-  
+      //show the total cost
+			var price = productArr[i-1].price;
+			var totalPrice = price * a;
+
+			console.log(" Total Cost: $" + totalPrice.toFixed(2))
+
+      exitOrCont();
+    });
 }
 
 function exitOrCont() {
 	inquirer.prompt([
 		{
 			type: "confirm",
-    	message: "Would you like to buy something else?",
+    	message: "\n Would you like to buy something else?",
     	name: "again"
   	}
 		]).then(answer => {
@@ -157,6 +137,6 @@ function exitOrCont() {
 			else{	
 				connection.end();	
 			}
-});
+	});
 }
 
